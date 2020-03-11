a9684792bfe801db98279721d82d24225aff896d
moarseniy <moarseniy@users.noreply.github.com>
2020-03-11 22:48:16 +0300

    Update matrixmult.cpp
    upload 1st step

--- matrixmult.cpp
+++ matrixmult.cpp
@@ -8,12 +8,12 @@ using namespace std;
 #define N 10000
 #define M 10000
 
-int* multipl(int** a, int* v, int n, int m)
+int* multipl1(int** a, int* v, int n, int m)
 {
     int i, j;
     int g = 0;
     int* v1 = new int[n];
-    #pragma omp parallel for
+    
     for (i = 0; i < n; i++)
     {
         g = 0;
@@ -23,6 +23,75 @@ int* multipl(int** a, int* v, int n, int m)
     }
     return v1;
 }
+int* multipl2(int** a, int* v, int n, int m)
+{
+    int i, j;
+    int g = 0;
+    int* v1 = new int[n];
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        for (i = 0; i < n/3; i++)
+        {
+            g = 0;
+            #pragma omp parallel for
+            for (j = 0; j < m; j++)
+                g += a[i][j] * v[j];
+            v1[i] = g;
+        }
+        #pragma omp section
+        for (i = n/3+1; i < 2*n/3; i++)
+        {
+            g = 0;
+            #pragma omp parallel for
+            for (j = 0; j < m; j++)
+                g += a[i][j] * v[j];
+            v1[i] = g;
+        }
+        #pragma omp section
+        for (i = 2*n/3+1; i < n; i++)
+        {
+            g = 0;
+            #pragma omp parallel for
+            for (j = 0; j < m; j++)
+                g += a[i][j] * v[j];
+            v1[i] = g;
+        }
+    }
+    return v1;
+}
+/*int* multipl2(int** a, int* v, int n, int m)
+{
+    int i, j;
+    int g = 0;
+    int* v1 = new int[n];
+    for (i = 0; i < n; i++)
+    {
+        g = 0;
+      #pragma omp parallel sections
+        {
+        #pragma omp section
+            {
+                for (j = 0; j < m / 3; j++)
+                    g += a[i][j] * v[j];
+                v1[i] = g;
+            }
+        #pragma omp section
+            {
+                for (j = m/3+1; j < 2*m/3; j++)
+                    g += a[i][j] * v[j];
+                v1[i] = g;
+            }
+        #pragma omp section
+            {
+                for (j = 2*m/3+1; j < m; j++)
+                    g += a[i][j] * v[j];
+                v1[i] = g;
+            }
+        }
+    }
+    return v1;
+}*/
 void printmas(int** a, int n, int m)
 {
     int i, j;
@@ -64,10 +133,14 @@ int main()
     //printvec(v, M); cout << endl;
     zapmatrix(a, N, M);
     unsigned int t1 = clock();
-    v1=multipl(a, v, N, M);
+    v1=multipl1(a, v, N, M);
     unsigned int t2 = clock();
-    cout << "Vremya " << t2 - t1 << endl;
+    cout << "Vremya normal " << t2 - t1 << endl;
 
+    unsigned int t3 = clock();
+    v1 = multipl2(a, v, N, M);
+    unsigned int t4 = clock();
+    cout << "Vremya parall " << t4 - t3 << endl;
     //printmas(a, N, M);
     cout << endl;
     //printvec(v1, N);
