ad3b911a711d580296b33a1eff8a57b7350bc133
moarseniy <moarseniy@users.noreply.github.com>
2020-05-03 18:45:04 +0300

    Create matrix gen, catch it!

---
+++ matrix_generator.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ofstream fout;
+    fout.open("matrix.txt");
+
+    int i, j, n, m, k=10, p=17;
+    cout << "N= ";
+    cin >> n;
+    cout << "M= ";
+    cin >> m;
+
+    int** a = new int* [n];
+    for (i = 0; i < n; i++)
+        a[i] = new int[m];
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            a[i][j] = 0;
+
+    for (i = 0; i < n; i=i+rand()%k)
+        for (j = 0; j < m; j=j+rand()%p)
+            a[i][j] = rand()%9;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (j != 0)
+                fout << " ";
+            fout << a[i][j];
+        }
+        fout << "\n";
+    }
+    fout.close();
+    return 0;
+    
+}
+
